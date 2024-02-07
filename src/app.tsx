b230518a7301b096071371c16f0b10a2ff8c9a35
Poveii <yellowtutors@gmail.com>
2024-02-07 19:53:18 -0300

    feat: Add `onNoteCreated` to create a note after `handleSaveNote`

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,29 @@
+import { useState } from 'react'
+
 import logo from './assets/logo-nlw-expert.svg'
 
 import { NewNoteCard } from './components/new-note-card'
 import { NoteCard } from './components/note-card'
 
+interface Note {
+  id: string
+  date: Date
+  content: string
+}
+
 export function App() {
+  const [notes, setNotes] = useState<Note[]>([])
+
+  function onNoteCreated(content: string) {
+    const newNote = {
+      id: crypto.randomUUID(),
+      date: new Date(),
+      content,
+    }
+    
+    setNotes([newNote, ...notes])
+  }
+
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6">
       <img src={logo} alt="NLW Expert" />
@@ -19,20 +39,11 @@ export function App() {
       <div className="h-px bg-slate-700" />
 
       <div className="grid grid-cols-3 auto-rows-[250px] gap-6">
-        <NewNoteCard />
+        <NewNoteCard onNoteCreated={onNoteCreated} />
         
-        <NoteCard note={{
-          date: new Date(),
-          content: "Testando"
-        }} />
-        <NoteCard note={{
-          date: new Date(),
-          content: "Teste"
-        }} />
-        <NoteCard note={{
-          date: new Date(),
-          content: "Hello World"
-        }} />
+        {notes.map((note) => {
+          return <NoteCard key={note.id} note={note} />
+        })}
       </div>
     </div>
   )
